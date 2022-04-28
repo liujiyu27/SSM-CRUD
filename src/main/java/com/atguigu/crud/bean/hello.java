6b2149999444b405a7022cb4c96b0cb962877cbc
jyu <jyu@atguigu.com>
2022-04-28 09:36:18 +0800

    second

---
+++ src/main/java/com/atguigu/crud/bean/hello.java
@@ -0,0 +1,14 @@
+package com.atguigu.crud.bean;
+
+
+import java.util.Arrays;
+
+/**
+ * @author liujiyu
+ * @since 2022-04-28 9:31
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
